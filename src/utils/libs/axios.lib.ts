d6da4f76e1aa586ddb9d2545456423bd9bec8cf3
Andrew G. Kagotho <andrewkagotho7@live.com>
2023-09-20 21:02:28 +0300

    Fixes

--- src/utils/libs/axios.lib.ts
+++ src/utils/libs/axios.lib.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
-const BASE_URL = 'http://localhost:3001/api'
+const BASE_URL = 'https://library-system-crud-4d7ba478356f.herokuapp.com/api'
 
 export default axios.create({
   baseURL: BASE_URL,
